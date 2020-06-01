7988ecf65d6b21617c171b34521ea48a5714e07d
Pha Kpa <kphaoctober@yahoo.com>
2020-06-01 17:27:52 -0400

    letter gen update

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,52 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const confirmLength = prompt("Choose a character length of password betweek 8-128: ");
+  const confirmLowercase = confirm("Lowercase?");
+  const confirmUppercase = confirm("Uppercase?");
+  const confirmNumber = confirm("Number?");
+  const confirmSpecialChar = confirm("SpecialCharacter?");
+
+  
+  console.log(confirmLength, confirmLowercase, confirmUppercase, confirmNumber, confirmSpecialChar);
+
+  function generatePassword() {
+    let tempPass = "";
+
+    for (let i =0; i < confirmLength; i++) {
+      tempPass += getRandomLower();
+    }
+    return tempPass;
+
+}
+
+
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  console.log(password);
+
+}
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!#$%&,()*+' -./:;<=\>?@[]^_`{|}~";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Add event listener to generate button
